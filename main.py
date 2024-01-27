22da92d1ed9b2b046d55ec800a1b84f6e63160b7
DESKTOP-9O3QD47\ASUS TUF <manh.tranduc@hcmut.edu.vn>
2024-01-27 17:34:14 +0700

    Lab 1

---
+++ main.py
@@ -0,0 +1,43 @@
+import sys
+from Adafruit_IO import MQTTClient
+import time
+import random
+AIO_FEED_IDs = ["nutnhan1", "nutnhan2"];
+AIO_USERNAME = "manhtrannnnnn"
+AIO_KEY = "aio_sIsb31K1EyYRtumbW7YLjr3Ry9su"
+
+def connected(client):
+    print("Ket noi thanh cong ...")
+    for topic in AIO_FEED_IDs:
+        client.subscribe(topic)
+
+def subscribe(client , userdata , mid , granted_qos):
+    print("Subscribe thanh cong ...")
+
+def disconnected(client):
+    print("Ngat ket noi ...")
+    sys.exit (1)
+
+def message(client , feed_id , payload):
+    print("Nhan du lieu: " + payload + ", feed_id: " + feed_id)
+
+client = MQTTClient(AIO_USERNAME , AIO_KEY)
+client.on_connect = connected
+client.on_disconnect = disconnected
+client.on_message = message
+client.on_subscribe = subscribe
+client.connect()
+client.loop_background()
+counter = 10
+while True:
+    counter = counter - 1
+    if counter <= 0:
+        counter = 10
+        temp = random.randint(10,40)
+        client.publish("cambien1", temp)
+        humi = random.randint(40,80)
+        client.publish("cambien2", humi)
+        lux = random.randint(0,500)
+        client.publish("cambien3", lux)
+    time.sleep(1)
+    pass
